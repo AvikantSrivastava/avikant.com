8cfefc8889ac961dcba6acd38823dcc4241d291d
Avikant Srivastava <asdavikant@gmail.com>
2023-12-30 21:42:20 +0530

    update: metatag

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://avikant.com",
+  author: "Avikant Srivastava",
+  desc: "Avikant Srivastava's Homepage",
   title: "home",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "avikant-srivastava-metatag.png",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
